d916939061693daecfaf3b49678160422a036f58
milyas1264 <milyasather42401@gmail.com>
2025-11-02 19:36:32 +0500

    update base path in vite.config.js to match product app

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  base: '/my-react-app/',
+  base: '/product-app/',
 
 })
