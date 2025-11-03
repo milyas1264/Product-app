2305e4f3b6762dbe1fb1c8e7ec4c01bd7765881e
milyas1264 <milyasather42401@gmail.com>
2025-11-03 07:05:38 +0500

    fix: update homepage URL and base path to match casing in package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  base: '/product-app/',
+  base: '/Product-app/',
 
 })
